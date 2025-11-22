b0ef1a21fd114fbbd6b75df997752c945d8a98be
bephemeral <244405225+bephemeral@users.noreply.github.com>
2025-11-21 21:56:44 -0500

    feat: shuttle hosting support

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::web::{self, ServiceConfig};
 use shorten_rs::{
     link::{create_link, get_link},
     state::AppState,
 };
+use shuttle_actix_web::ShuttleActixWeb;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[shuttle_runtime::main]
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // Note: web::Data created _outside_ HttpServer::new closure
     let state = web::Data::new(AppState::new());
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(state.clone())
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.app_data(state.clone())
             .service(get_link)
-            .service(create_link)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+            .service(create_link);
+    };
+
+    Ok(config.into())
 }
