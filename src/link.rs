3aaed872a573eead4ad10f52e1b11928c23d8c97
bephemeral <244405225+bephemeral@users.noreply.github.com>
2025-12-07 13:00:49 -0500

    (BREAKING) refac: LinkNew/Link architecture

--- src/link.rs
+++ src/link.rs
@@ -1,26 +1,53 @@
+use crate::state::AppState;
+use actix_web::web::Data;
+use rand::distr::{Alphanumeric, SampleString};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
 use url::{ParseError, Url};
 
-pub struct Link {
+#[derive(Deserialize)]
+pub struct LinkNew {
     url: String,
 }
 
+#[derive(Serialize, FromRow)]
+pub struct Link {
+    id: String,
+    url: String,
+}
+
+fn get_unique_id(data: &Data<AppState>) -> String {
+    let id = Alphanumeric.sample_string(&mut rand::rng(), 4);
+
+    if data.redirects.contains_key(&id) {
+        get_unique_id(data)
+    } else {
+        id
+    }
+}
+
 impl Link {
-    pub fn new(url: &str) -> Result<Link, ()> {
-        let link = match Url::parse(url) {
+    pub fn new(data: &Data<AppState>, link: LinkNew) -> Result<Link, ()> {
+        let link = match Url::parse(&link.url) {
             Ok(parsed) => {
                 // allow only http and https
                 match parsed.scheme() {
-                    "http" | "https" => parsed.into(),
+                    "http" | "https" => parsed.to_string(),
                     _ => return Err(()), // not really accurate but close enough
                 }
             }
-            Err(ParseError::RelativeUrlWithoutBase) => {
-                format!("http://{}", url)
-            }
+            Err(ParseError::RelativeUrlWithoutBase) => format!("http://{}", link.url),
             Err(_) => return Err(()),
         };
 
-        Ok(Link { url: link })
+        Ok(Link {
+            id: get_unique_id(data),
+            url: link,
+        })
+    }
+
+    pub fn id(&self) -> &str {
+        self.id.as_str()
     }
 
     pub fn url(&self) -> &str {
--- src/routes.rs
+++ src/routes.rs
@@ -1,22 +1,8 @@
-use crate::{link::Link, state::AppState};
+use crate::{
+    link::{Link, LinkNew},
+    state::AppState,
+};
 use actix_web::{HttpResponse, Responder, get, post, web};
-use rand::distr::{Alphanumeric, SampleString};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct LinkPayload {
-    url: String,
-}
-
-fn get_unique_id(data: &web::Data<AppState>) -> String {
-    let id = Alphanumeric.sample_string(&mut rand::rng(), 4);
-
-    if data.redirects.contains_key(&id) {
-        get_unique_id(data)
-    } else {
-        id
-    }
-}
 
 #[get("/{id}")]
 pub async fn get_link(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
@@ -29,19 +15,16 @@ pub async fn get_link(path: web::Path<String>, data: web::Data<AppState>) -> imp
         .finish()
 }
 
+/*
 #[post("/create")]
-pub async fn create_link(
-    payload: web::Json<LinkPayload>,
-    data: web::Data<AppState>,
-) -> impl Responder {
-    let link = match Link::new(&payload.url) {
+pub async fn create_link(payload: web::Json<LinkNew>, data: web::Data<AppState>) -> impl Responder {
+    let link = match Link::new(&data, payload.into_inner()) {
         Ok(l) => l,
         Err(_) => return HttpResponse::BadRequest().body("Invalid URL"),
     };
 
-    let id = get_unique_id(&data);
+    data.redirects.insert(link.id().to_string(), link);
 
-    data.redirects.insert(id.clone(), link);
-
-    HttpResponse::Ok().json(id)
+    HttpResponse::Created().json(link)
 }
+*/
